58545111f9ad322977b214267282abed31064f59
Leonardo Ravello Aguado <leonardoravello1999@gmail.com>
2024-06-13 02:11:45 -0500

    agregar mysql servidor

--- src/main/java/com/tienda/controllers/CompraController.java
+++ src/main/java/com/tienda/controllers/CompraController.java
@@ -5,12 +5,14 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.tienda.entities.Carrito;
 import com.tienda.entities.ItemCarrito;
@@ -21,7 +23,8 @@ import com.tienda.services.IItemCarritoService;
 import com.tienda.services.IProductoService;
 import com.tienda.services.IVentaService;
 
-@Controller
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
 @RequestMapping("/api/home")
 public class CompraController {
 
